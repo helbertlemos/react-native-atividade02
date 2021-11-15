182d842008302b2b39913ee0241e1ab245c827b1
helbertlemos <helblemos@gmail.com>
2021-11-15 15:16:26 -0300

    save

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, View, Text } from 'react-native';
+import styled from 'styled-components/native';
 
+const Div = styled.View`
+  width: 150px;
+  height: 150px;
+  flex-direction: row;
+  justify-content: center;
+`;
+const Quadrado = styled.View`
+  width: 150px;
+  height:150px;
+  
+`;
+const Externo = styled.View`
+width: 150px;
+height:150px;
+`;
+const Body = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+`;
+const P = styled.Text`
+  color: red;
+  font-size: 20px;
+`;
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Body>
+      <P>Galeria do Helbert Wagner Sales Lemos</P>
+      <P>Tema: trollge</P>
+      <Div>
+        <Quadrado>
+          <Image source={require('./src/images/1.png')} 
+            style={{ width: 150, height: 150 }}
+            resizeMode='stretch'
+        /></Quadrado>
+        <Quadrado>
+          <Image source={require('./src/images/2.png')} 
+            style={{ width: 150, height: 150 }}
+            resizeMode='stretch'
+        /></Quadrado>
+      </Div>
+      <Div>
+      <Quadrado>
+          <Image source={require('./src/images/3.jpg')} 
+            style={{ width: 150, height: 150 }}
+            resizeMode='stretch'
+        /></Quadrado>
+        <Quadrado>
+          <Image source={require('./src/images/4.jpg')} 
+            style={{ width: 150, height: 150 }}
+            resizeMode='stretch'
+        /></Quadrado>
+      </Div>
+      <Externo>
+        <Image source={require('./src/images/zone.png')} 
+          style={{ width: 150, height: 150 }}
+          resizeMode='stretch'
+        />
+      </Externo>
+    </Body>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
